398b1e6ce72b7fc531f6c1ecb33ce56c4a809782
Robert Evans <robertevans@robertevansmb.com>
2014-12-19 15:31:22 -0500

    Remove to_soap_array functin

--- lib/odfl_freight.rb
+++ lib/odfl_freight.rb
@@ -6,14 +6,4 @@ class OdflFreight
     instance_variables.each {|var| hash[var.to_s.delete("@")] = instance_variable_get(var) }
     hash
   end
-
-  def to_soap_array
-    result=Array.new
-
-    instance_variables.each do |var|
-      result.push({var.to_s.delete("@") => Integer(instance_variable_get(var))})
-    end
-
-    result
-  end
 end
