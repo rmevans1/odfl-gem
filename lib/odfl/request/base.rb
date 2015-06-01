3921bf2ce42fdc967e9ef51ffbc426482f7d48b7
Robert Evans <robertevans@robertevansmb.com>
2015-06-01 15:26:23 -0400

    Add RATED_CLASS

---
+++ lib/odfl/request/base.rb
@@ -0,0 +1,8 @@
+module Odfl
+  module Request
+    class Base
+      # List of available freight classes
+      RATED_CLASS = %w(50 55 60 65 70 77 85 92 100 110 125 150 175 200 250 300 400 500)
+    end
+  end
+end
---
+++ lib/odfl/request/rate.rb
@@ -0,0 +1,9 @@
+require 'odfl/request/base'
+
+module Odfl
+  module Request
+    class Rate < Base
+
+    end
+  end
+end
